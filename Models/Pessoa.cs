deec5fe854fc6e81a70cd31b826b043f81e30e05
LeoST25 <matheusleonardo224@gmail.com>
2024-05-15 02:20:07 -0300

    Propriedades, Métodos e Construtores com C#

---
+++ Models/Pessoa.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        private string _nome;
+        public string Nome 
+        { 
+            get => _nome.ToUpper();
+            
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+            }
+        }
+
+
+        
+        private int _idade;
+        public int Idade 
+        { 
+            get => _idade;
+            
+
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que zero");
+                }
+
+                _idade = value;
+            }
+        }
+        public string Sobrenome { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Leonardo", sobrenome: "Matheus");
+Pessoa p2 = new Pessoa(nome: "Maria", sobrenome: "Joaquina");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Pessoa p1 = new Pessoa();
+p1.Nome = "Leonardo";
+p1.Sobrenome = "Matheus";
+p1.Idade = 25;
+p1.Apresentar(); */
