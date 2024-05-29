e753b4fe0f2e8272aae97f063fb9a4286fbe8136
LeoST25 <matheusleonardo224@gmail.com>
2024-05-28 21:52:12 -0300

    Exceções e Coleções

--- Program.cs
+++ Program.cs
@@ -1,7 +1,260 @@
 ﻿using ExemploExplorando.Models;
 using System.Globalization;
 
-string dataString = "2024-05-16 14:04";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+estados.Add("AL", "Alagoas");
+
+Console.WriteLine(estados["MG"]); */
+
+
+/* foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");    
+}
+
+Console.WriteLine("-------------");
+
+estados.Remove("BA");
+estados["SP"] = "São Paulo - Valor alterado";
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");    
+} */
+
+/* string chave = "BA2";
+Console.WriteLine($"Verificando o elemento: {chave}");
+
+if (estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente: {chave}");
+}
+else
+{
+    Console.WriteLine($"Valor não existe. É seguro adicionar a chave: {chave}");
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Stack<int> pilha = new Stack<int>();
+
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);   
+}
+
+Console.WriteLine($"Removendo o elemento: {pilha.Pop()}");
+
+pilha.Push(20);
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);   
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* new ExemploExcecao().Metodo1(); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* string[] linhasExemplo = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+
+Console.WriteLine("Chegou até aqui"); */
+
+/* try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+} 
+finally
+{
+    Console.WriteLine("Chegou até aqui");
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* string dataString = "2024-05-16 14:04";
 
 bool sucesso = DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
 
@@ -12,7 +265,7 @@ if (sucesso)
 else
 {
     System.Console.WriteLine($"{dataString} não é uma data válida");
-}
+} */
 
 
 
